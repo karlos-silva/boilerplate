082425c91e3da361f5ccbbeda9855263f52a8771
Karlos Silva <karlossilva194@gmail.com>
2022-03-13 22:11:48 -0300

    chore: add typescript on project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../sttyles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({title = 'teste'}: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +17,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Te amo <a href="https://nextjs.org">{title}</a>
         </h1>
 
         <p className={styles.description}>
